950fccf3f27f57057370546d3b997d445ed2ef39
Logan Oram <162460537+oram79@users.noreply.github.com>
2025-03-23 15:53:47 -0230

    fixed error in repo

--- src/main/java/com/keyin/member/MemberRepository.java
+++ src/main/java/com/keyin/member/MemberRepository.java
@@ -1,12 +1,13 @@
 package com.keyin.member;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface MemberRepository extends CrudRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long> {
     List<Member> findByMemberName(String name);
     List<Member> findByPhoneNumber(String phoneNumber);
 }
--- src/main/java/com/keyin/tournament/TournamentRepository.java
+++ src/main/java/com/keyin/tournament/TournamentRepository.java
@@ -1,6 +1,7 @@
 package com.keyin.tournament;
 
 import com.keyin.member.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,7 +9,7 @@ import java.util.Date;
 import java.util.List;
 
 @Repository
-public interface TournamentRepository extends CrudRepository<Tournament, Long> {
+public interface TournamentRepository extends JpaRepository<Tournament, Long> {
     List<Tournament> findByDateStart(Date startDate);
     List<Tournament> findByLocation(String location);
 }
