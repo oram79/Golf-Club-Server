3a5af50dddac46a7891c3f21197bc3c28a9fea7c
Logan Oram <logan.oram@keyin.com>
2025-03-24 12:16:37 -0230

    repository

--- src/main/java/com/keyin/member/MemberRepository.java
+++ src/main/java/com/keyin/member/MemberRepository.java
@@ -3,11 +3,18 @@ package com.keyin.member;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface MemberRepository extends CrudRepository<Member, Long> {
-    Iterable<Member> findMemberByTournamentId(Long tournamentId);
-    Optional<Member> findByMemberID(Long memberId);
-    Optional<Member> findByName(Long name);
+    Optional<Member> findMemberByMemberName(String memberName);
+
+    Optional<List<Member>> findMemberByMembershipType(String membershipType);
+
+    Optional<Member> findMemberByPhoneNumber(String phoneNumber);
+
+    Optional<List<Member>> findMemberByTournaments_StartDate(String startDate);
+
+    Optional<List<Member>> findMemberByTournaments_TournamentName(String tournamentName);
 }
--- src/main/java/com/keyin/tournament/TournamentRepository.java
+++ src/main/java/com/keyin/tournament/TournamentRepository.java
@@ -1,16 +1,18 @@
 package com.keyin.tournament;
 
-import com.keyin.member.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Date;
-import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface TournamentRepository extends CrudRepository<Tournament, Long> {
-    Optional<Tournament> findByDateStart(Date startDate);
+public interface TournamentRepository extends CrudRepository<Tournament, Long>  {
+
+    public Tournament findByTournamentName(String tournamentName);
+
+    Optional<Tournament> findByTournamentId(Long tournamentId);
+
+    Optional<Tournament> findByStartDate(String startDate);
+
     Optional<Tournament> findByLocation(String location);
 }
