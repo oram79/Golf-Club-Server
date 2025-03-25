5ee4ec06338dd16c027a9013d7f9ee4b7f64302f
Logan Oram <logan.oram@keyin.com>
2025-03-25 13:53:08 -0230

    final repository

--- src/main/java/com/keyin/member/MemberRepository.java
+++ src/main/java/com/keyin/member/MemberRepository.java
@@ -3,13 +3,12 @@ package com.keyin.member;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MemberRepository extends CrudRepository<Member, Long> {
     public Member findByName(String name);
-
-    public Member findByMembershipType(String membershipType);
-
+    public List<Member> findByMembershipType(String membershipType);
     public Member findByPhoneNumber(String phoneNumber);
-
     public Member findMemberByMembershipStartDate(String membershipStartDate);
 }
--- src/main/java/com/keyin/tournament/TournamentRepository.java
+++ src/main/java/com/keyin/tournament/TournamentRepository.java
@@ -3,12 +3,8 @@ package com.keyin.tournament;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Calendar;
-import java.util.List;
-
 @Repository
 public interface TournamentRepository extends CrudRepository<Tournament, Long> {
     public Tournament findByStartDate(String startDate);
-
     public Tournament findByLocation(String location);
 }
